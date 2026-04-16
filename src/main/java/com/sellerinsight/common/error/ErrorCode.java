c245fafbbb5b63d85a54371c0f0321b722064495
Eom Sanghyun <quarterMoon234@gmail.com>
2026-04-16 21:09:28 +0900

    task: 에러 코드 필드 수정

--- src/main/java/com/sellerinsight/common/error/ErrorCode.java
+++ src/main/java/com/sellerinsight/common/error/ErrorCode.java
@@ -11,12 +11,11 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "COMMON_400", "요청 값이 올바르지 않습니다."),
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON_404", "요청한 리소스를 찾을 수 없습니다."),
     DUPLICATE_RESOURCE(HttpStatus.CONFLICT, "COMMON_409", "이미 존재하는 리소스입니다."),
-
     SELLER_CREDENTIAL_NOT_FOUND(HttpStatus.NOT_FOUND, "SELLER_404_1", "판매자 연동 정보를 찾을 수 없습니다."),
 
-    NAVER_COMMERCE_SIGNATURE_FAILED(HttpStatus.BAD_REQUEST, "COMMERCE_400_1", "네이버 커머스 clientSecret 형식이 올바르지 않습니다."),
-    NAVER_COMMERCE_AUTH_FAILED(HttpStatus.BAD_GATEWAY, "COMMERCE_502_1", "네이버 커머스 인증 토큰 발급에 실패했습니다."),
-    NAVER_COMMERCE_AUTH_INVALID_RESPONSE(HttpStatus.BAD_GATEWAY, "COMMERCE_502_2", "네이버 커머스 인증 응답 형식이 올바르지 않습니다."),
+    IMPORT_JOB_NOT_FOUND(HttpStatus.NOT_FOUND, "IMPORT_404_1", "가져오기 작업을 찾을 수 없습니다."),
+    CSV_IMPORT_INVALID_FILE(HttpStatus.BAD_REQUEST, "IMPORT_400_1", "CSV 파일이 비어 있거나 형식이 올바르지 않습니다."),
+    CSV_IMPORT_FAILED(HttpStatus.BAD_REQUEST, "IMPORT_400_2", "CSV 가져오기에 실패했습니다."),
 
     DB_CONNECTION_FAILED(HttpStatus.SERVICE_UNAVAILABLE, "HEALTH_503", "데이터베이스 연결에 실패했습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500", "서버 내부 오류가 발생했습니다.");
