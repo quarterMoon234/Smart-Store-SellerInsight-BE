a0babbb35029a9e357fd7b69d9574300d2fd2597
Eom Sanghyun <quarterMoon234@gmail.com>
2026-04-16 11:15:15 +0900

    레포지토리 등록용 첫 커밋

---
+++ src/main/java/com/sellerinsight/back/SellerinsightApplication.java
@@ -0,0 +1,13 @@
+package com.sellerinsight.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerinsightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellerinsightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sellerinsight/SellerinsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellerinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerinsightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
