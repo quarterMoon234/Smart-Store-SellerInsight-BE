71d17fd0792448e4853a3e49583b788a6e5d9193
Eom Sanghyun <quarterMoon234@gmail.com>
2026-04-20 10:51:49 +0900

    feat: 상품 레포지토리 수정

--- src/main/java/com/sellerinsight/product/domain/ProductRepository.java
+++ src/main/java/com/sellerinsight/product/domain/ProductRepository.java
@@ -2,9 +2,14 @@ package com.sellerinsight.product.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findBySellerIdAndExternalProductId(Long sellerId, String externalProductid);
+
+    List<Product> findAllBySellerId(Long sellerId);
+
+    long countBySellerIdAndStockQuantityLessThanEqual(Long sellerId, Integer stockQuantity);
 }
