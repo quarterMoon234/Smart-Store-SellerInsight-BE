398cfc669d4859bc4db71ea24275a71f89567e18
Eom Sanghyun <quarterMoon234@gmail.com>
2026-04-20 10:52:02 +0900

    feat: 주문 레포지토리 수정

--- src/main/java/com/sellerinsight/order/domain/CustomerOrderRepository.java
+++ src/main/java/com/sellerinsight/order/domain/CustomerOrderRepository.java
@@ -2,9 +2,17 @@ package com.sellerinsight.order.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Long> {
 
     Optional<CustomerOrder> findBySellerIdAndExternalOrderNo(Long sellerId, String externalOrderNo);
+
+    List<CustomerOrder> findAllBySellerIdAndOrderedAtGreaterThanEqualAndOrderedAtLessThan(
+            Long sellerId,
+            OffsetDateTime start,
+            OffsetDateTime end
+    );
 }
