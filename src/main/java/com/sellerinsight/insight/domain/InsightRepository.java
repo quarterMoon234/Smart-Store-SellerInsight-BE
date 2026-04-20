2b82b7f7152822399fcd70d07e54f8b5551d9736
Eom Sanghyun <quarterMoon234@gmail.com>
2026-04-20 19:14:49 +0900

    feat: 인사이트 레포지토리 수정

--- src/main/java/com/sellerinsight/insight/domain/InsightRepository.java
+++ src/main/java/com/sellerinsight/insight/domain/InsightRepository.java
@@ -1,5 +1,6 @@
 package com.sellerinsight.insight.domain;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -15,6 +16,10 @@ public interface InsightRepository extends JpaRepository<Insight, Long> {
     );
 
     List<Insight> findAllBySellerIdAndMetricDateOrderByIdAsc(Long sellerId, LocalDate metricDate);
+
+    List<Insight> findAllBySellerId(Long sellerId, Pageable pageable);
+
+    long countBySellerIdAndSeverity(Long sellerId, InsightSeverity severity);
 
     Optional<Insight> findByIdAndSellerId(Long id, Long sellerId);
 }
